c9bbfb3edab948e034ddec7fe55fd2ae0e31b172
rahul <rahul.baxi@gmail.com>
2013-03-11 03:17:00 -0700

    first commit

---
+++ src/com/example/helloandroid/DisplayMessageActivity.java
@@ -0,0 +1,35 @@
+package com.example.helloandroid;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends Activity{
+	
+	@SuppressLint("NewApi")
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		Intent intent = getIntent();
+		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+		TextView textView = new TextView(this);
+		textView.setText(message);
+		
+		setContentView(textView);
+		
+	}
+	
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
